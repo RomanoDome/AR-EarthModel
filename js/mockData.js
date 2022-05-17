59b86fd2b80b35b3b2977ae1b50fca67030f82ec
RomanoDome <49345710+RomanoDome@users.noreply.github.com>
2022-05-17 16:55:44 +0200

    Add files via upload

---
+++ js/mockData.js
@@ -0,0 +1,95 @@
+export const mockData = {
+    points: [
+        {
+            id: 0,
+            latitude: (Math.random()-0.5)*180,
+            longitude: (Math.random()-0.5)*180,
+            title: "Test",
+            subtitle: "My test subtitle"
+        },
+        {
+            id: 1,
+            latitude: (Math.random()-0.5)*180,
+            longitude: (Math.random()-0.5)*180,
+            title: "Test 2",
+            subtitle: "My test subtitle 2"
+        },
+        {
+            id: 2,
+            latitude: (Math.random()-0.5)*180,
+            longitude: (Math.random()-0.5)*180,
+            title: "Test 3",
+            subtitle: "My test subtitle 3"
+        },
+        {
+            id: 3,
+            latitude: (Math.random()-0.5)*180,
+            longitude: (Math.random()-0.5)*180,
+            title: "Test 4",
+            subtitle: "My test subtitle 4"
+        },
+        {
+            id: 4,
+            latitude: (Math.random()-0.5)*180,
+            longitude: (Math.random()-0.5)*180,
+            title: "Test 5",
+            subtitle: "My test subtitle 5"
+        },
+        {
+            id: 5,
+            latitude: (Math.random()-0.5)*180,
+            longitude: (Math.random()-0.5)*180,
+            title: "Test 6",
+            subtitle: "My test subtitle 6"
+        },
+        {
+            id: 6,
+            latitude: (Math.random()-0.5)*180,
+            longitude: (Math.random()-0.5)*180,
+            title: "Test 7",
+            subtitle: "My test subtitle 7"
+        },
+        {
+            id: 7,
+            latitude: (Math.random()-0.5)*180,
+            longitude: (Math.random()-0.5)*180,
+            title: "Test 8",
+            subtitle: "My test subtitle 8"
+        },
+        {
+            id: 8,
+            latitude: (Math.random()-0.5)*180,
+            longitude: (Math.random()-0.5)*180,
+            title: "Test 9",
+            subtitle: "My test subtitle 9"
+        },
+        {
+            id: 9,
+            latitude: (Math.random()-0.5)*180,
+            longitude: (Math.random()-0.5)*180,
+            title: "Test 10",
+            subtitle: "My test subtitle 10"
+        },
+        {
+            id: 10,
+            latitude: (Math.random()-0.5)*180,
+            longitude: (Math.random()-0.5)*180,
+            title: "Test 11",
+            subtitle: "My test subtitle 11"
+        },
+        {
+            id: 11,
+            latitude: (Math.random()-0.5)*180,
+            longitude: (Math.random()-0.5)*180,
+            title: "Test 12",
+            subtitle: "My test subtitle 12"
+        },
+        {
+            id: 12,
+            latitude: (Math.random()-0.5)*180,
+            longitude: (Math.random()-0.5)*180,
+            title: "Test 13",
+            subtitle: "My test subtitle 13"
+        }
+    ]
+}
