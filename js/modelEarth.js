dee0d4a126d671968e74e178c93e2ae158190d84
RomanoDome <49345710+RomanoDome@users.noreply.github.com>
2022-05-17 17:07:39 +0200

    Add files via upload

--- js/modelEarth.js
+++ js/modelEarth.js
@@ -28,11 +28,12 @@ function init()
 	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
     camera.position.z = MAX_CAMERA_Z;
 
-	renderer = new THREE.WebGLRenderer();
+	renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	document.body.appendChild( renderer.domElement );
 
     cameraControl = new OrbitControls(camera, renderer.domElement);
+    cameraControl.enableDamping = true;
     cameraControl.minDistance = MIN_CAMERA_Z;
     cameraControl.maxDistance = MAX_CAMERA_Z;
     
@@ -68,25 +69,11 @@ function init()
     scene.add( hemiLightHelper );
     scene.add( hemiLight );
 
-    const dirLight = new THREE.DirectionalLight( 0xffffff, 1 );
-    dirLight.color.setHSL( 0.1, 1, 0.95 );
-    dirLight.position.set( - 1, 1.75, 1 );
-    dirLight.position.multiplyScalar( 30 );
-    scene.add( dirLight );
-
-    dirLight.castShadow = true;
-    dirLight.shadow.mapSize.width = 2048;
-    dirLight.shadow.mapSize.height = 2048;
-
-    const d = 50;
-
-    dirLight.shadow.camera.left = - d;
-    dirLight.shadow.camera.right = d;
-    dirLight.shadow.camera.top = d;
-    dirLight.shadow.camera.bottom = - d;
-
-    dirLight.shadow.camera.far = 3500;
-    dirLight.shadow.bias = - 0.0001; 
+    let light = new THREE.AmbientLight(0xffffff, 0.5);
+    scene.add(light);
+    light = new THREE.SpotLight(0xffffff, 0.8);
+    light.position.x = 100;
+    scene.add(light);
 
     mockData.points.map(point => addPoint(point));         
 }
